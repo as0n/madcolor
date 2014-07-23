b2ff7cd7683cea9a48ef930d68b104ed53e91029
as0n <n.was.here@gmail.com>
2014-07-23 11:18:16 +0800

    Changed madcolor application methods : automatic for .mc and revamped options mangement.

--- README.md
+++ README.md
@@ -4,32 +4,51 @@ A psychedelic improvement for any webpage.
 
 ## How to
 
+### Easy way (html)
+
+Tell madcolor what element you want to target with the `mc` class :
+
+```html
+<div class="mc" id="myPsychedelicDiv"></div>
+```
+
 Include `madcolor.js` in your html :
 
 ```html
 <script type="text/javascript" src="madcolor.js"></script>
 ```
 
-And call `madcolor()` on any HTMLElement you want !
+And you're done !
 
-```javascript
-madcolor(document.getElementById("myPsychedelicDiv"));
-```
+### Programmatic way (javascript)
+
+You can also use `madcolor(el, options)` to active the madcolor effect on that element.
+Argument `el` is an HTMLElement and `options` is an object described below.
 
 ## Options
 
-You can also specify various options using :
+It's also possible to pass options to madcolor, either via element attributes (html way) or via an object (javascript way).
+
+Examples :
+
+```html
+<div class="mc" mc-color-period="0.5" mc-color-range="180"></div>
+```
 
 ```javascript
-madcolor(element, {
-	colorPeriod : 10,
-	[...]
+madcolor(myPsychedelicElement, {
+	colorPeriod : 0.5,
+	colorRange : 180
 });
 ```
 
- - `colorPeriod` (float/int) : how long (in seconds) between two random colors. The longest the smoothest. The shortest the **psychedelic-est** !
- - `displayPeriod` (float/int) : how long (in seconds) between two `background-color` transitions. This is kind of memory consumming.
- - `listSize` (int) : the bigger, the smoother transitions will be, but it'll also be more memory-consumming !
- - `showMap` (boolean) : whether to display a color map in the bottom right corner of the targeted element.
- - `showHexCode` (boolean) : whether to display the current color code (in hex) in the bottom left corner of the targeted element.
- - `colorRange` (int[0-255]) : the smaller, the darker the colors will be (useful for changing backgrounds without having to re-think your entire color design ...).
+### Options list
+
+Attribute name | Object Name | Type | Description
+-------------- | ----------- | ---- | -----------
+mc-color-period | `colorPeriod` | float / int | how long (in seconds) between two random colors. The longest the smoothest. The shortest the **psychedelic-est** !
+mc-display-period | `displayPeriod` | float / int | how long (in seconds) between two `background-color` transitions. This is kind of memory consumming.
+mc-list-size | `listSize` | int | the bigger, the smoother transitions will be, but it'll also be more memory-consumming !
+mc-show-map | `showMap` | boolean | whether to display a color map in the bottom right corner of the targeted element.
+mc-show-hexcode | `showHexCode` | boolean | whether to display the current color code (in hex) in the bottom left corner of the targeted element.
+mc-color-range | `colorRange` | int[0-255] | the smaller, the darker the colors will be (useful for changing backgrounds without having to re-think your entire color design ...).
--- madcolor.js
+++ madcolor.js
@@ -1,11 +1,35 @@
 madcolor = (function() {
-	defaultOptions = {
-		colorPeriod : 2,
-		displayPeriod : 0.2,
-		listSize : 5,
-		showMap : true,
-		showHexCode : true,
-		colorRange : 255
+	var defOptions = {
+		colorPeriod : {
+			default : 2,
+			attr : "mc-color-period",
+			parser : parseFloat
+		},
+		displayPeriod : {
+			default : 0.2,
+			attr : "mc-display-period",
+			parser : parseFloat
+		},
+		listSize : {
+			default : 5,
+			attr : "mc-list-size",
+			parser : parseInt
+		},
+		showMap : {
+			default : true,
+			attr : "mc-show-map",
+			parser : function(el) { if (el === "true") return true; if (el === "false") return false; return null; }
+		},
+		showHexCode : {
+			default : true,
+			attr : "mc-show-hexcode",
+			parser : function(el) { if (el === "true") return true; if (el === "false") return false; return null; }
+		},
+		colorRange : {
+			default : 255,
+			attr : "mc-color-range",
+			parser : parseInt
+		}
 	};
 
 	function rand(n) {
@@ -142,116 +166,128 @@ madcolor = (function() {
 		};
 	}
 
-	return {
-		apply : function(el, opt) {
-			var options = {},
-				poly = {
-					r : null,
-					g : null,
-					b : null
-				},
-				pHexCode,
-				cnvsSet, cnvsBg, cnvsTrace,
-				ctxBg, ctxTrace,
-				cnvsHeight, cnvsWidth,
-				lastColorUpdate,
-				lastX, lastY;
+	function apply(el, opt) {
+		var options = {},
+			poly = {
+				r : null,
+				g : null,
+				b : null
+			},
+			pHexCode,
+			cnvsSet, cnvsBg, cnvsTrace,
+			ctxBg, ctxTrace,
+			cnvsHeight, cnvsWidth,
+			lastColorUpdate,
+			lastX, lastY;
 
-			el.className += " mc";
+		if (!opt) opt = {};
+		for (key in defOptions) {
+			options[key] = (opt[key]!==undefined) ? opt[key] : defOptions[key].default;
+		}
 
-			if (!opt) opt = {};
-			for (key in defaultOptions) {
-				options[key] = opt[key]!==undefined ? opt[key] : defaultOptions[key];
+		var listOffset = Math.floor(options.listSize/2);
+
+		if (options.showHexCode) {
+			pHexCode = document.createElement("p");
+			pHexCode.className = "caption hexCode";
+			el.appendChild(pHexCode);
+		}
+		if (options.showMap) {
+			var container = document.createElement("div");
+			container.className = "caption map";
+			cnvsSet = document.createElement("div");
+			cnvsSet.id = "canvaslist";
+			container.appendChild(cnvsSet);
+			el.appendChild(container);
+			cnvsHeight = cnvsSet.clientHeight;
+			cnvsWidth = cnvsSet.clientWidth;
+
+			cnvsBg = document.createElement("canvas");
+			ctxBg = cnvsBg.getContext("2d");
+			cnvsTrace = document.createElement("canvas");
+			ctxTrace = cnvsTrace.getContext("2d");
+
+			cnvsTrace.height = cnvsBg.height = cnvsHeight;
+			cnvsTrace.width = cnvsBg.width = cnvsWidth;
+			ctxTrace.fillStyle = ctxTrace.strokeStyle = "#ffffff";
+			ctxTrace.lineWidth = 2;
+
+			cnvsSet.appendChild(cnvsBg);
+			cnvsSet.appendChild(cnvsTrace);
+
+			for (var i = 0; i<cnvsWidth; i++) {
+				for (var j = 0; j<cnvsHeight; j++) {
+					var h = Math.round(i*360/cnvsWidth),
+						v = 1 - j/cnvsHeight;
+					ctxBg.fillStyle = Color.fromHSV(h, 1, v).toString();
+					ctxBg.fillRect(i, j, 1, 1);
+				}
 			}
+		}
 
-			var listOffset = Math.floor(options.listSize/2);
+		while (Color.list.length < options.listSize) {
+			Color.list.push(Color.random(options.colorRange));
+		}
 
-			if (options.showHexCode) {
-				pHexCode = document.createElement("p");
-				pHexCode.className = "caption hexCode";
-				el.appendChild(pHexCode);
-			}
+		function setColor(col) {
+			var s = col.toString();
+			if (options.showHexCode) pHexCode.textContent = s;
+			el.style.backgroundColor = s;
+		}
+
+		el.style.transitionDuration = options.displayPeriod+"s";
+
+		function colorUpdate() {
+			Color.list.shift();
+			Color.list.push(Color.random(options.colorRange));
+			poly = Polynomial.fromColorList(Color.list, listOffset);
+			lastColorUpdate = new Date();
+		}
+
+		function display() {
+			var t = (new Date() - lastColorUpdate)/(options.colorPeriod*1000),
+				col = new Color(poly.r.calc(t), poly.g.calc(t), poly.b.calc(t)),
+				hsv = col.toHSV(),
+				x = (hsv.h/360)*cnvsWidth,
+				y = (1 - hsv.v)*cnvsHeight;
+			setColor(col);
+
 			if (options.showMap) {
-				var container = document.createElement("div");
-				container.className = "caption map";
-				cnvsSet = document.createElement("div");
-				cnvsSet.id = "canvaslist";
-				container.appendChild(cnvsSet);
-				el.appendChild(container);
-				cnvsHeight = cnvsSet.clientHeight;
-				cnvsWidth = cnvsSet.clientWidth;
-
-				cnvsBg = document.createElement("canvas");
-				ctxBg = cnvsBg.getContext("2d");
-				cnvsTrace = document.createElement("canvas");
-				ctxTrace = cnvsTrace.getContext("2d");
-
-				cnvsTrace.height = cnvsBg.height = cnvsHeight;
-				cnvsTrace.width = cnvsBg.width = cnvsWidth;
-				ctxTrace.fillStyle = ctxTrace.strokeStyle = "#ffffff";
-				ctxTrace.lineWidth = 2;
-
-				cnvsSet.appendChild(cnvsBg);
-				cnvsSet.appendChild(cnvsTrace);
-
-				for (var i = 0; i<cnvsWidth; i++) {
-					for (var j = 0; j<cnvsHeight; j++) {
-						var h = Math.round(i*360/cnvsWidth),
-							v = 1 - j/cnvsHeight;
-						ctxBg.fillStyle = Color.fromHSV(h, 1, v).toString();
-						ctxBg.fillRect(i, j, 1, 1);
-					}
+				imgdata = ctxTrace.getImageData(0,0,cnvsWidth,cnvsHeight);
+				for (var i = 0; i<imgdata.data.length/4; i++) {
+					imgdata.data[4*i+3] = Math.max(0, imgdata.data[4*i+3]-10);
 				}
+				ctxTrace.clearRect(0,0,cnvsWidth,cnvsHeight);
+				ctxTrace.putImageData(imgdata, 0, 0);
+				//ctxTrace.fillRect(x, y, 3, 3);
+				ctxTrace.beginPath();
+				ctxTrace.moveTo(lastX, lastY);
+				ctxTrace.lineTo(x, y);
+				ctxTrace.stroke();
 			}
+			lastX = x;
+			lastY = y;
+		}
 
-			while (Color.list.length < options.listSize) {
-				Color.list.push(Color.random(options.colorRange));
-			}
+		colorUpdate();
+		setInterval(colorUpdate, options.colorPeriod*1000);
+		setInterval(display, options.displayPeriod*1000);
+	}
 
-			function setColor(col) {
-				var s = col.toString();
-				if (options.showHexCode) pHexCode.textContent = s;
-				el.style.backgroundColor = s;
-			}
+	var targets = document.getElementsByClassName('mc');
+	for (var i = 0; i<targets.length; i++) {
+		var el = targets[i],
+			opts = {},
+			val;
 
-			el.style.transitionDuration = options.displayPeriod+"s";
+		for (var key in defOptions) {
+			val = el.getAttribute(defOptions[key].attr)
+			if (val) opts[key] = defOptions[key].parser(val);
+		}
 
-			function colorUpdate() {
-				Color.list.shift();
-				Color.list.push(Color.random(options.colorRange));
-				poly = Polynomial.fromColorList(Color.list, listOffset);
-				lastColorUpdate = new Date();
-			}
+		apply(targets[i], opts);
+	}
 
-			function display() {
-				var t = (new Date() - lastColorUpdate)/(options.colorPeriod*1000),
-					col = new Color(poly.r.calc(t), poly.g.calc(t), poly.b.calc(t)),
-					hsv = col.toHSV(),
-					x = (hsv.h/360)*cnvsWidth,
-					y = (1 - hsv.v)*cnvsHeight;
-				setColor(col);
-
-				if (options.showMap) {
-					imgdata = ctxTrace.getImageData(0,0,cnvsWidth,cnvsHeight);
-					for (var i = 0; i<imgdata.data.length/4; i++) {
-						imgdata.data[4*i+3] = Math.max(0, imgdata.data[4*i+3]-10);
-					}
-					ctxTrace.clearRect(0,0,cnvsWidth,cnvsHeight);
-					ctxTrace.putImageData(imgdata, 0, 0);
-					//ctxTrace.fillRect(x, y, 3, 3);
-					ctxTrace.beginPath();
-					ctxTrace.moveTo(lastX, lastY);
-					ctxTrace.lineTo(x, y);
-					ctxTrace.stroke();
-				}
-				lastX = x;
-				lastY = y;
-			}
-
-			colorUpdate();
-			setInterval(colorUpdate, options.colorPeriod*1000);
-			setInterval(display, options.displayPeriod*1000);
-		},
-		Color : Color
-	};
+	apply.color = Color;
+	return apply;
 })();
